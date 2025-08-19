c3468e0bebd967f7a94b1d50bbca2448f577923d
whatsappbot0325-hub <whatsappbot0325@gmail.com>
2025-08-20 03:46:58 +0530

    Update session.js

--- session.js
+++ session.js
@@ -1,38 +1,3 @@
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                   𝗤𝗨𝗘𝗘𝗡-𝗔𝗡𝗝𝗨 𝗫ᴾᴿᴼ 𝐁𝐎𝐓                                               //
-//                                                                                                      //
-//                                         Ｖ：4.0                                                       //
-//
-//
-//░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░██╗░░░░░██╗██╗░░░██╗  ██╗░░██╗██████╗░██████╗░░█████╗░
-//██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░██║░░░░░██║██║░░░██║  ╚██╗██╔╝██╔══██╗██╔══██╗██╔══██╗
-//██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔██╗██║░░░░░██║██║░░░██║  ░╚███╔╝░██████╔╝██████╔╝██║░░██║
-//╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚████║██╗░░██║██║░░░██║  ░██╔██╗░██╔═══╝░██╔══██╗██║░░██║
-//░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚███║╚█████╔╝╚██████╔╝  ██╔╝╚██╗██║░░░░░██║░░██║╚█████╔╝
-//░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░╚═════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░                                           
-//
-//
-//
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//*
-//  * @project_name : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ
-//  * @version      : 4.0
-//  * @author       : Janith Rashmika
-//  * @youtube      : https://www.youtube.com/@gamingrash2006
-//  * @description  : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ, A Multi-functional WhatsApp bot created by Janith Rashmika.
-//*
-//*
-//Base by Janith Rashmika
-//GitHub: @Mrrashmika
-//WhatsApp: +94717775628
-//Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@gamingrash2006
-//   * Created By GitHub: Mrrashmika
-//   * Credit To Janith Rashmika
-//   * © 2025 © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ-V4.
-// ⛥┌┤
-// */
-
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -40,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibUpNM1RVd2RPTFZoNVZ0TGlJV3BmcmF5SmtCR3lGVmZDTUo2bHI4T1VHTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZEZQR25FUGd3VHBvaStBajNYMkgxak9VUXVXeVZWTnpsTnpjQ3FiZStGTT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBQi9QbDNaNVA4WGFhVVpJTUlBSDVtNXY3bmxhR1ptd0lYcEMxaXlLYkdBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSMEhwSERuL2VRTzBxelBtM1FPMVlUbU1XbEdpd2lHc1E2ZVRWRTQvcUM4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImlGRXdPN0RKRWZyd1VUeG9Hb3RwSFhiVHJBazZoMDhZSzFDNk00YzBVMTA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJuaThFSCt2UTZ2YXVLczBzUUdHSWpUMHkyY0hkM0RrMCthV01Tekl0aFk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUdMcUJncjNmSFVmYTV5T1VCNWhQeXdHbGc1TlRmMjRZWnNZOGNLbmxHUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ2p6bG41Ujg3bnNCWFdZMjVLaml0czdLUEFuZFNRSjVVcW84a0FOMmtDbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ii9MWURLa0c4WmZqdjdHb081bHh1YWM4VUhaMTRxT0xKQWJGMmpDcVpXZkVlZ0tWT3Mzc3R3WGRwWUVCNWNmQ01iRzliWkxzUWlsTGtWRWE3aTN3QWp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjA4LCJhZHZTZWNyZXRLZXkiOiJJYVROUlZHMHRDQVUzZE5jK01YN3RveEVPd0c3a1cyMkNBaDF5REpzTXdRPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6Ijk0NzE3Nzc1NjI4QHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IjIxNTBGRjdCOUJCNDM0NkUzNTlDNkQ2RTZFMDgwNTA5In0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3NTUxNzY4Mzd9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6InVoaWRRT3prVHZXMHZsYTV1OHBnTEEiLCJwaG9uZUlkIjoiYjQ4ZGU3NDQtYjdlOS00YmVhLTg4OWEtNmQ5NmJmMzA2YWM5IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZqOTY1STcrYUlSOTg0YW9zMUY5K0U3bG5Qaz0ifSwicmVnaXN0ZXJlZCI6ZmFsc2UsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkMrRzVUOXF6bDRLZDlXcDF4dWpYc1lrcStNPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTVRoaG9NRkVQKys5OFFHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiMjhyQitDUnlQMHA0QjVjSVpDYzBYUXNEZ042bThjM1ZhSjV2OU5nT2FtTT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiSVlLZlZjdGRUSWtXYUdUbnhnWlY4enlFZ1pNYUZ6REZ0K2lUeC9iWHBaV3QxUS9UeUlBMG5xRmcvK1FKZmIvcVROU1VzWEN5Z3VlL0lyei9Vam1JRFE9PSIsImRldmljZVNpZ25hdHVyZSI6Ik1acTR2WHA5dlZGc29taWkwN3hBdWV5UU9saG4vRGV2dHpzRjJPdUxMMC9CV2FURG4yNlRkQjhtcmptUFBiK1ErMmRjTlJoeU1XVTlpTlQyQ0o0TWdBPT0ifSwibWUiOnsiaWQiOiI5NDcxNzc3NTYyODoxOEBzLndoYXRzYXBwLm5ldCIsImxpZCI6IjIxNDgzOTE0NjMzNjI5NzoxOEBsaWQiLCJuYW1lIjoi4bS84bS44bSwIPCdmbrwnZm48J2ZvfCdmbYg8J2agfCdmbDwnZqC8J2Zt/CdmbzwnZm48J2ZuvCdmbAifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3MTc3NzU2Mjg6MThAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZHZLd2Zna2NqOUtlQWVYQ0dRbk5GMExBNERlcHZITjFXaWViL1RZRG1waiJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FnSUFnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU1MTc2ODMzLCJsYXN0UHJvcEhhc2giOiIyVjc3cVUiLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUJsUCJ9",
+SESSION_ID: process.env.SESSION_ID || "dila~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYU4zUXhnc0Q0UCtiZW5oTWVOeWY0RHN5eUs1Wkc0L0lxOVhpZVJJSDQwaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieU1pSmdCUFhINlk5aG10d2xIUnA1NlVodWY5bEc4bVhaVUlrQ1o5eFgzOD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI0QXl2QzNLTDhKZllRaHhpT0phY1FxdEZaNDVIVCtyemFsZjZSOWNHNjJBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhcHY0WFQwZ1I4VWNackV0ZlJScHlwUVdxdlFMY0JjRzNNMkFqV3dVUjJ3PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdNc2hxTXdIL1RhbWpQeGpwbEF4dlZFWDJsaERMYVE0dmc1RE9aZmY3MDg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IllpaWU3VExtU2E0M1hmWWYvQkNSTDRWSmZkQk13dytuU1NpdGt5NzN4MG89In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0ovNzRoM3BFZE5uWWFZR2JMSmo0emVmMGVld3hTNnE4Rmh3RXcxSXdsVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNDRnNmR5UjRwUnU1M1NpTHhxc1RpQnZKVXJxNXVPOTV2SkxmQkJha1ZBVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjF3L2c3eVJnUFAxK0ZOOHZibDN6VDM5R2l1QXRiNGhsY1l4Yll1RElyMjlIU1E0Q3pBMXczck1NUlZtY0tHTkVDeGFtSWEvVFNwZEIzTmw0QlZ0bWh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MywiYWR2U2VjcmV0S2V5IjoiK0VyZHRFbFhMeTFKcHRBSmVCbldOUUtWSnlwaEYrTjZRSWhCMW1BWExrZz0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiUHVUdDllTUlSUWlMM2ZVNHB4NEhGdyIsInBob25lSWQiOiI3N2ZkNjQwZS01NDBiLTRjNjMtOWRjMC1hZmI3OTVhNTcwNmMiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidVFLNjNUT3l0blJRTFlYeHVEcVJLMHp6dHBnPSJ9LCJyZWdpc3RlcmVkIjp0cnVlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjN4OVFOV2RVam90dDREWXM0NUhxVGVib1BzQT0ifSwicmVnaXN0cmF0aW9uIjp7fSwicGFpcmluZ0NvZGUiOiI2QzQ4S0VRUyIsIm1lIjp7ImlkIjoiOTQ3NzYyODM1NzQ6MTFAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxNzU3MTYyMzg5NDYzODE6MTFAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKT3B3TUFGRUt5YmtjVUdHQVFnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJMYytGYW5ZYkhJdEdqQndweWc5aW1tWkZudnhjNVAwRi83K2d3amlFT0hFPSIsImFjY291bnRTaWduYXR1cmUiOiJscDJ3cTFYTnIzZGlmdXA3WURpVncvSVU4YzN4V0FqSzN3MmExYUY1MFEvTUhidTR1SU1hTXExR2NkZXdnQlU1OGpFSElueGlQaUg2Q05xMXpGa3pCZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiaEZoRmNjY3Z1NHY0U1ZWT2x1MHdkZmNCRWdUMFl4U25NY0FrRzNoTXh5blRQZzZZTHVPTDdVTDBnYi9pMWVCclVJU1JQQ2xzM0M0QTV4L1U1cjdWanc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDc3NjI4MzU3NDoxMUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJTM1BoV3AyR3h5TFJvd2NLY29QWXBwbVJaNzhYT1Q5QmYrL29NSTRoRGh4In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQUlJRFE9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NTU1OTgyNjcsImxhc3RQcm9wSGFzaCI6IjJWNzdxVSIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBT0ltIn0=",
 PORT: process.env.PORT || "8000"
 };
